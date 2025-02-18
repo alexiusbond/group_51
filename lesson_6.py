9b0c3538ac39d54bfcca350957083a4f060165b2
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2025-02-18 18:45:20 +0600

    lesson 6 codes

--- lesson_6.py
+++ lesson_6.py
@@ -1,1 +1,31 @@
-# here will be code of lesson 6
+animals = ['cat', 'dog', 'rabbit', 'cow', 'tiger']
+fruits = ['apple', 'banana', 'orange']
+
+for animal in animals:
+    print(animal)
+
+for fruit in fruits:
+    print(fruit)
+# O(A + F)
+
+for animal in animals:
+    for fruit in fruits:
+        print(f'{animal} loves {fruit}')
+# O(A * F)
+
+num = 0
+while num < len(animals):
+    for fruit in fruits:
+        print(fruit)
+    for animal in animals:
+        print(animal)
+    num += 1
+
+# O((F + A) * A) = > O(FA + A**2)
+
+def counter(n):
+    print(n)
+    if n > 0:
+        counter(n - 1)
+
+counter(3)
